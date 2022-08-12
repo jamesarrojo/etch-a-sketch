d693969f78e106db47dbc8e9c8eb6a9711ecf19b
jamesarrojo <jamesarrojo.01@gmail.com>
2022-08-12 22:22:57 +0800

    Use 'this' keyword to select element to be colored

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("container")
 
-console.log(container)
+// console.log(container)
 
 // function createDiv() {
     
@@ -14,19 +14,48 @@ console.log(container)
 
 // createDiv()
 
-function createGrid() {
-    for (let i = 0; i < 16; i++){
+
+
+
+function createGrid(dimension) {
+    for (let i = 0; i < dimension; i++){
         const row = document.createElement('div')
-        row.classList.add(`row-${i+1}`)
+        row.classList.add(`column-${i+1}`)
         container.appendChild(row)
         
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < dimension; j++) {
             const box = document.createElement('div')
+            box.style.setProperty("width", `${35 / dimension}vw`)
+            box.style.setProperty("height", `${35 / dimension}vw`)
             box.classList.add('box')
+            // box.setAttribute("data-key", `${i}${j}`)
             row.append(box)
+            // const boxTwo = document.querySelector('.box')
+            // console.log(boxTwo)
+            // console.log(box)
+
 
         }
     }
 }
 
-createGrid()
+function main() {
+    // const userInput = Number(prompt('Please enter the grid dimensions'))
+    let userInput = 16
+    createGrid(userInput)
+}
+
+
+
+main()
+
+
+
+const box = document.querySelectorAll(".box")
+function changeColor() {
+    this.classList.add('colored')
+    // console.log(this.getAttribute('data-key'))
+}
+box.forEach(e => {
+    e.addEventListener("mouseover", changeColor)
+})
